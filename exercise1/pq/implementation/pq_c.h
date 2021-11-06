307525b29e63ac283b990c8930c614133de09333
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-11-06 17:16:06 +0100

    Add pq implementations

--- exercise1/pq/implementation/pq_c.h
+++ exercise1/pq/implementation/pq_c.h
@@ -2,22 +2,90 @@
 
 #include <queue>
 #include <utility>
+#include <cassert>
 
 template<class T, class Comp = std::less<T> >
 class PriQueueC
 {
 public:
-    PriQueueC(std::size_t capacity, std::size_t log_degree = 3) { }
+    PriQueueC(std::size_t capacity, std::size_t log_degree = 3) : m_log_degree{log_degree} { m_elements.reserve(capacity); }
 
-    const T&    top()   const { return pq.top();   }
-    bool        empty() const { return pq.empty(); }
-    std::size_t size()  const { return pq.size();  }
+    const T &top() const {
+        assert(!empty());
+        return m_elements.front();
+    }
 
-    void push(T value) { return pq.push(std::move(value)); }
-    void pop ()        { return pq.pop();       }
+    bool empty() const { return m_elements.empty(); }
+
+    std::size_t size() const { return m_elements.size(); }
+
+    void push(T value) {
+        m_elements.push_back(value);
+
+        size_t i = size() - 1;
+        while (i > 0) {
+            auto p = parent(i);
+            if (m_comp(m_elements[i], m_elements[p])) {
+                break;
+            } else {
+                std::swap(m_elements[p], m_elements[i]);
+                assert(m_comp(m_elements[i], m_elements[parent(i)]));
+                i = p;
+            }
+        }
+    }
+
+    void pop() {
+        auto const degree = static_cast<size_t>(1) << m_log_degree;
+
+        assert(!empty());
+
+        m_elements[0] = std::move(m_elements.back());
+        m_elements.pop_back();
+
+        if (size() == 0)
+            return;
+
+        size_t i = 0;
+        while (true) {
+            assert(i < size());
+
+            size_t max_idx = i;
+            for (size_t k = child(i, 0); k < std::min(child(i, degree - 1) + 1, size()); ++k) {
+                if (m_comp(m_elements[max_idx], m_elements[k])) {
+                    max_idx = k;
+                }
+            }
+            if (i == max_idx)
+                break;
+            std::swap(m_elements[i], m_elements[max_idx]);
+
+            i = max_idx;
+        }
+    }
+
+private:
+    [[nodiscard]] constexpr size_t child(size_t i, size_t j) const {
+        auto const degree = static_cast<size_t>(1) << m_log_degree;
+        return i * degree + j + 1;
+    }
+
+    [[nodiscard]] constexpr size_t parent(size_t i) const {
+        return (i - 1) >> m_log_degree;
+    }
+
+    [[nodiscard]] bool is_valid() const {
+        for (size_t i = 1; i < size(); ++i) {
+            if (!m_comp(m_elements[i], m_elements[parent(i)])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 private:
     /* member definitions *****************************************************/
-    std::priority_queue<T, std::vector<T>, Comp> pq;
-    // T* or std::vector<T>
+    std::vector<T> m_elements;
+    std::size_t m_log_degree;
+    Comp m_comp;
 };
