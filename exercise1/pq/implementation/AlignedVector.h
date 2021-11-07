944bab4036e33dce9eaf212cfcfe98071db0baa4
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-11-07 17:34:44 +0100

    Add reserve method

--- exercise1/pq/implementation/AlignedVector.h
+++ exercise1/pq/implementation/AlignedVector.h
@@ -3,8 +3,8 @@
 template<class T, bool throw_exceptions = false>
 class AlignedVector {
 public:
-    explicit AlignedVector(std::size_t capacity, std::size_t block_size = 1, std::size_t offset = 0)
-            : m_capacity(capacity), m_size(0) {
+    explicit AlignedVector(std::size_t capacity = 0, std::size_t block_size = 1, std::size_t offset = 0)
+            : m_block_size(block_size), m_offset(offset) {
         assert(block_size != 0);
         assert(offset < block_size);
         if (throw_exceptions && block_size == 0) {
@@ -13,14 +13,45 @@ public:
         if (throw_exceptions && offset >= block_size) {
             throw std::invalid_argument("offset must be less than block_size");
         }
-        if (capacity > 0) {
-            m_alloc_ptr = static_cast<T *>(std::aligned_alloc(alignof(T) * block_size,
-                                                              sizeof(T) * (capacity + block_size)));
-            if (m_alloc_ptr == nullptr) {
+        reserve(capacity);
+    }
+
+    void reserve(std::size_t new_cap) {
+        if (new_cap > m_capacity) {
+            const auto alignment = next_power_of_two(block_alignment(m_block_size));
+            const auto size = ((sizeof(T) * (new_cap + m_offset) + alignment - 1) / alignment) * alignment;
+
+            assert(is_power_of_two(alignment) && "alignment must be a power of two");
+            assert(is_multiple_of(size, alignment) && "size must be an integer multiple of alignment");
+
+            auto *alloc_ptr = static_cast<T *>(std::aligned_alloc(alignment, size));
+
+            if (alloc_ptr == nullptr) {
                 throw std::bad_alloc();
             }
-            m_elements = m_alloc_ptr + offset;
+
+            auto *elements = alloc_ptr + m_offset;
+
+            if (m_alloc_ptr != nullptr) {
+                std::uninitialized_move_n(m_elements, m_size, elements);
+
+                free(m_alloc_ptr);
+                m_alloc_ptr = nullptr;
+                m_elements = nullptr;
+            }
+
+            m_alloc_ptr = alloc_ptr;
+            m_elements = elements;
+            m_capacity = size / sizeof(T) - m_offset;
         }
+        assert(m_alloc_ptr != nullptr);
+        assert(m_elements != nullptr);
+        assert(m_capacity >= new_cap);
+        assert(m_size <= m_capacity);
+    }
+
+    [[nodiscard]] static constexpr std::size_t block_alignment(std::size_t block_size) noexcept {
+        return alignof(T) * block_size;
     }
 
     AlignedVector(const AlignedVector &other) = delete;
@@ -32,26 +63,39 @@ public:
     AlignedVector &operator=(AlignedVector &&other) = delete;
 
     ~AlignedVector() {
-        while (!empty()) {
-            pop_back();
+        if (m_alloc_ptr != nullptr) {
+            std::destroy_n(m_elements, m_size);
+            free(m_alloc_ptr);
+            m_alloc_ptr = nullptr;
+            m_elements = nullptr;
+            m_capacity = 0;
+            m_size = 0;
         }
-        free(m_alloc_ptr);
-        m_alloc_ptr = nullptr;
-        m_elements = nullptr;
-        m_capacity = 0;
-        m_size = 0;
+
+        assert(m_alloc_ptr == nullptr);
+        assert(m_elements == nullptr);
+        assert(m_capacity == 0);
+        assert(m_size == 0);
     }
 
-    [[nodiscard]] constexpr bool empty() const {
+    [[nodiscard]] constexpr bool empty() const noexcept {
         return m_size == 0;
     }
 
-    [[nodiscard]] constexpr std::size_t size() const {
+    [[nodiscard]] constexpr std::size_t size() const noexcept {
         return m_size;
     }
 
-    [[nodiscard]] constexpr std::size_t capacity() const {
+    [[nodiscard]] constexpr std::size_t capacity() const noexcept {
         return m_capacity;
+    }
+
+    [[nodiscard]] constexpr T *data() noexcept {
+        return m_elements;
+    }
+
+    [[nodiscard]] constexpr const T *data() const noexcept {
+        return m_elements;
     }
 
     T &operator[](std::size_t index) {
@@ -109,8 +153,30 @@ public:
     }
 
 private:
+    [[nodiscard]] static constexpr std::size_t next_power_of_two(std::size_t value) noexcept {
+        if (value <= 1) {
+            return 1;
+        }
+        std::size_t k = 2;
+        value--;
+        while (value >>= 1) {
+            k <<= 1;
+        }
+        return k;
+    }
+
+    [[nodiscard]] static constexpr bool is_power_of_two(std::size_t value) noexcept {
+        return (value & (value - 1)) == 0;
+    }
+
+    [[nodiscard]] static constexpr bool is_multiple_of(std::size_t value, std::size_t multiple) noexcept {
+        return value == (value / multiple) * multiple;
+    }
+
     T *m_alloc_ptr{nullptr};
     T *m_elements{nullptr};
     std::size_t m_capacity{0};
     std::size_t m_size{0};
+    std::size_t m_block_size{1};
+    std::size_t m_offset{0};
 };
