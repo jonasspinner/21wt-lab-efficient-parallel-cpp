15bda0f8025f5e3f58fd192b01adeb15632f2cd7
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-11-19 17:58:42 +0100

    Fix minor dikstra details

--- exercise2/implementation/dijkstra.hpp
+++ exercise2/implementation/dijkstra.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <cassert>
+
 #include "indexed_priority_queue.hpp"
 
 // Construct your Dijkstra implementation here.  It should be used by first
@@ -23,15 +25,12 @@ private:
 
     const GraphType &graph;
 
-    std::vector<NodeHandle> frontier{};
-    std::vector<NodeHandle> next_frontier{};
     std::vector<double> distance{};
-    IndexedPriorityQueue<std::size_t, double, std::greater<>> queue;
+    IndexedPriorityQueue<decltype(graph.nodeId(graph.node(0))), double, std::greater<>> queue;
 
+    static_assert(std::is_same_v<decltype(graph.nodeId(graph.node(0))), std::size_t>);
 public:
-    DijkstraHelper(const GraphType &graph) : graph(graph) {
-        frontier.reserve(graph.numNodes());
-        next_frontier.reserve(graph.numNodes());
+    explicit DijkstraHelper(const GraphType &graph) : graph(graph) {
         queue.reserve(graph.numNodes());
     }
 
@@ -51,7 +50,8 @@ public:
         distance[start_id] = 0.0;
 
         while (!queue.empty()) {
-            auto [u, d_u] = queue.pop();
+            auto [u_id, d_u] = queue.pop();
+            auto u = graph.node(u_id);
             assert(d_u < infty);
 
             if (u == end) {
@@ -70,72 +70,6 @@ public:
                     queue.pushOrChangePriority(v_id, d_v_from_u);
                     distance[v_id] = d_v_from_u;
                 }
-            }
-        }
-        return infty;
-    }
-
-
-    double dijkstra2(NodeHandle start, NodeHandle end) {
-        constexpr auto infty = std::numeric_limits<double>::infinity();
-
-        if (start == end) {
-            return 0.0;
-        }
-
-        auto start_id = graph.nodeId(start);
-
-        queue.clear();
-        queue.push(start_id, 0.0);
-
-        distance.assign(graph.numNodes(), infty);
-        distance[start_id] = 0.0;
-
-        while (!queue.empty()) {
-            auto [u, d_u] = queue.pop();
-            assert(d_u < infty);
-
-            if (u == end) {
-                return d_u;
-            }
-
-            for (auto e = graph.beginEdges(u); e < graph.endEdges(u); ++e) {
-                auto v = graph.edgeHead(e);
-                auto w = graph.edgeWeight(e);
-                auto v_id = graph.nodeId(v);
-
-                auto d_v = distance[v_id];
-                auto d_v_from_u = d_u + w;
-
-                if (queue.hasKey(v_id)) {
-                    assert(d_v == queue.priority(v_id));
-                }
-
-                if (d_v == infty) {
-                    queue.push(v_id, d_v_from_u);
-                    distance[v_id] = d_v_from_u;
-                } else if (d_v_from_u < d_v) {
-                    queue.changePriority(v_id, d_v_from_u);
-                    distance[v_id] = d_v_from_u;
-                }
-
-                /*
-                if (d_v_from_u < d_v) {
-                    queue.pushOrChangePriority(v_id, d_v_from_u);
-                    distance[v_id] = d_v_from_u;
-                }
-                 */
-
-                /*
-                if (queue.hasKey(v_id)) {
-                    auto d_v = queue.priority(v_id);
-                    if (d_v_from_u < d_v) {
-                        queue.changePriority(v_id, d_v_from_u);
-                    }
-                } else {
-                    queue.push(v_id, d_v_from_u);
-                }
-                 */
             }
         }
         return infty;
