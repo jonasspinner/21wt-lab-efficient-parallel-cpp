78493260742121e5ee1326827d92b0e1b17b768f
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-11-19 15:53:13 +0100

    Add naive explicit graph

--- exercise2/implementation/node_graph.hpp
+++ exercise2/implementation/node_graph.hpp
@@ -2,6 +2,10 @@
 
 #include <cstddef>
 #include <vector>
+#include <cassert>
+#include <memory>
+#include <numeric>
+#include <cstdint>
 
 #include "edge_list.hpp"
 
@@ -16,31 +20,62 @@
 // But if you want to implement an iterator you can find a blueprint in exc1
 
 
-class NodeGraph {
+template<class Index = uint64_t>
+class NodeGraphT {
  public:
     struct Node {
-        std::vector<Node*> neighbors;
+        Index id{0};
+        std::vector<const Node*> neighbors{};
+        explicit Node(Index id) : id(id) {}
     };
 
-    using NodeHandle = /* TODO */;
-    using EdgeIterator = /* TODO */;
+    using NodeHandle = const Node*;
+    using EdgeIterator = typename std::vector<const Node*>::const_iterator;
 
-    NodeGraph() = default;
-    NodeGraph(std::size_t num_nodes, const EdgeList& edges);
-    ~NodeGraph();
+    NodeGraphT() = default;
+    NodeGraphT(std::size_t num_nodes, const EdgeList& edges) {
+        nodes_.reserve(num_nodes);
+        for (std::size_t id = 0; id < num_nodes; ++id) {
+            nodes_.push_back(std::make_unique<Node>(id));
+        }
+        assert(nodes_.size() == num_nodes);
+        for (const auto &e : edges) {
+            NodeHandle n = nodes_[e.to].get();
+            assert(n != nullptr);
+            nodes_[e.from]->neighbors.push_back(n);
+        }
+    }
 
-    std::size_t numNodes() const;
+    [[nodiscard]] std::size_t numNodes() const {
+        return nodes_.size();
+    }
 
-    NodeHandle node(std::size_t n) const;
-    std::size_t nodeId(NodeHandle n) const;
+    [[nodiscard]] NodeHandle node(std::size_t id) const {
+        assert(id < numNodes());
+        return nodes_[id].get();
+    }
 
-    EdgeIterator beginEdges(NodeHandle) const;
-    EdgeIterator endEdges(NodeHandle) const;
+    [[nodiscard]] std::size_t nodeId(NodeHandle n) const {
+        assert(n != nullptr);
+        return n->id;
+    }
 
-    NodeHandle edgeHead(EdgeIterator) const;
-    double edgeWeight(EdgeIterator) const
+    [[nodiscard]] EdgeIterator beginEdges(NodeHandle n) const {
+        return n->neighbors.begin();
+    }
+    [[nodiscard]] EdgeIterator endEdges(NodeHandle n) const {
+        return n->neighbors.end();
+    }
+
+    [[nodiscard]] NodeHandle edgeHead(EdgeIterator e) const {
+        return *e;
+    }
+
+    [[nodiscard]] double edgeWeight(EdgeIterator) const
     { return 1.; /* no weighted implementation */ }
 
  private:
-    std::vector<Node*> nodes_;
+    std::vector<std::unique_ptr<Node>> nodes_;
 };
+
+using NodeGraph = NodeGraphT<>;
