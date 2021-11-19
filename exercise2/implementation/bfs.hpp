a06ac665732fbbd71451a2a48894c4ea52ef89ba
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-11-19 15:54:40 +0100

    Add breadth-first search

--- exercise2/implementation/bfs.hpp
+++ exercise2/implementation/bfs.hpp
@@ -13,24 +13,61 @@
   auto distance   = nodeHelper.bfs(handle1, handle2);
 */
 
-template <class GraphClass>
-class BFSHelper
-{
+template<class GraphClass>
+class BFSHelper {
 private:
-    using GraphType    = GraphClass;
-    using NodeHandle   = typename GraphType::NodeHandle;
+    using GraphType = GraphClass;
+    using NodeHandle = typename GraphType::NodeHandle;
     using EdgeIterator = typename GraphType::EdgeIterator;
 
-    const GraphType& graph;
+    const GraphType &graph;
 
-    // TODO maybe other data members
+    std::vector<NodeHandle> frontier{};
+    std::vector<NodeHandle> next_frontier{};
+    std::vector<bool> visited{};
 
 public:
-    BFSHelper(const GraphType& graph) : graph(graph) { }
+    explicit BFSHelper(const GraphType &graph) : graph(graph) {
+        frontier.reserve(graph.numNodes());
+        next_frontier.reserve(graph.numNodes());
+        visited.reserve(graph.numNodes());
+    }
 
-    std::size_t bfs(NodeHandle start, NodeHandle end)
-    {
-        // TODO implementation
+    std::size_t bfs(NodeHandle start, NodeHandle end) {
+        if (start == end) {
+            return 0;
+        }
+
+        frontier.clear();
+        frontier.push_back(start);
+        next_frontier.clear();
+        visited.assign(graph.numNodes(), false);
+
+        visited[graph.nodeId(start)] = true;
+
+        std::size_t distance = 1;
+
+        while (!frontier.empty()) {
+            while (!frontier.empty()) {
+                NodeHandle n = std::move(frontier.back());
+                frontier.pop_back();
+
+                for (EdgeIterator e = graph.beginEdges(n); e < graph.endEdges(n); ++e) {
+                    NodeHandle neighbor = graph.edgeHead(e);
+                    if (neighbor == end) {
+                        return distance;
+                    }
+                    auto id = graph.nodeId(neighbor);
+                    if (!visited[id]) {
+                        visited[id] = true;
+                        next_frontier.push_back(neighbor);
+                    }
+                }
+            }
+            distance++;
+            std::swap(frontier, next_frontier);
+        }
+        return std::numeric_limits<std::size_t>::max();
     }
 
 };
